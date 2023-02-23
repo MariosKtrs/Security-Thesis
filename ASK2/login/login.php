1d24ce08f26b106c561735a50a76c859841ccdbc
MariosKtrs <mskatsaros@outlook.com>
2023-02-23 23:46:22 +0200

    vulnerability and fix added for exercise 2

--- ASK2/login/login.php
+++ ASK2/login/login.php
@@ -6,45 +6,40 @@ session_start();
 	include("functions.php");
 
 
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+		
 		//something was posted
 		$user_name = $_POST['user_name'];
 		$password = $_POST['password'];
 
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-		{
+		//read from database
 
-			//read from database
-			$query = "select * from users where user_name = '$user_name' limit 1";
-			$result = mysqli_query($con, $query);
+		//safe
+		//$query = "select * from users where user_name = '$user_name'";
+			
+		//vulnerable
+		$query = "select user_id from users where user_name = '$user_name' and password='$password'";
+			
+		$result = mysqli_query($con, $query);
 
-			if($result)
-			{
-				if($result && mysqli_num_rows($result) > 0)
-				{
+		if($result){
+			
+			if(mysqli_num_rows($result) > 0){
 
-					$user_data = mysqli_fetch_assoc($result);
-					
-					if($user_data['password'] === $password)
-					{
+				$user_data = mysqli_fetch_assoc($result);
 
-						$_SESSION['user_id'] = $user_data['user_id'];
-						header("Location: index.php");
-						die;
-					}
-				}
+				//if ($user_data['password']=== $password){
+					$_SESSION['user_id'] = $user_data['user_id'];
+					header("Location: index.php");
+					die;
+				//}
 			}
+		}
 			
 			echo "wrong username or password!";
-		}else
-		{
-			echo "wrong username or password!";
-		}
 	}
 
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
