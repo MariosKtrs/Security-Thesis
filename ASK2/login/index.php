9b7a928804892b4f7abcdf32b9bebdc58939117c
MariosKtrs <mskatsaros@outlook.com>
2023-03-08 01:09:31 +0200

    optimizations and bug fixes

--- ASK2/login/index.php
+++ ASK2/login/index.php
@@ -13,22 +13,23 @@ session_start();
         <style><?php include "index.css" ?></style>     
     </head>
     <body>
-        <a class="logout" href="logout.php">Logout</a>
-        <h1>Welcome to your Super Secure Account <?php echo $user_data['user_name']?>!</h1>
-        <form class="index-box" method="post">
-            <p>Want to stay informed and get updates? Just enter your email below!</p>
-            <div class="search">
-                <input class="search-bar" name="sender" type="email">
-                <input class="submit" type="submit" name="submit" value="Search"> 
-            </div>
-            <p id="error-text"></p>
-            <?php 
-                if(isset($_POST['submit'])){ 
-                    $name = $_POST['sender'];  
-                    echo "<p> Email $name successfully added!</p>";
-                } 
-            ?>
-        </form>
+        <div class="top">
+            <a class="logout" href="logout.php">Logout</a>
+            <h1>Welcome to your Super Secure Account <?php echo $user_data['user_name']?>!</h1>
+            <form class="index-box" method="post">
+                <p>Enter your name below and we'll say hi!</p>
+                <div class="search">
+                    <input class="search-bar" name="sender" type="text">
+                    <input class="submit" type="submit" name="submit" value="Submit"> 
+                </div>
+               <p> <?php 
+                    if(isset($_POST['submit'])){ 
+                        $name = $_POST['sender'];  
+                        echo "<p> Hello $name !</p>";
+                    } 
+                ?> </p>
+            </form>
+        </div>
         <img src="./images/index_bg.jpg">
     </body>
 </html>
