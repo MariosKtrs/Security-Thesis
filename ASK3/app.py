ddca440f0c973c8db835e28bd56e3321dd72eb27
MariosKtrs <mskatsaros@outlook.com>
2023-03-30 03:14:11 +0300

    Exercise 3 completed

--- ASK3/app.py
+++ ASK3/app.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, render_template
-from jinja2 import Environment, Markup
-import os
+from flask import Flask, request, render_template_string, render_template
+import os;
 
 app = Flask(__name__)
-Jinja2 = Environment()
+
 
 @app.route('/')
 def home():
-    name = "admin"
-    tags = Jinja2.from_string('Welcome Back ' + str(request.values.get('name'))).render()
-    return render_template('index.html',tags=tags) 
+    name = ""
+    tags = request.args.get('name');
+    if request.args.get('name'):
+        return render_template_string('Wait! This is top secret! '+request.args.get('name')) 
+    else:
+        return render_template('index.html') 
     
 if __name__ == "__main__":
+    app.config['SECRET_KEY'] = 'MXD{W4wrov_Hsrh4}' 
     port = int(os.environ.get('PORT', 5050))
     app.run(debug=True, host='0.0.0.0', port=port)
