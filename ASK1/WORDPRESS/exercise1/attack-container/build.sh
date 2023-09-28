6ccbd3021f4d2d82cb33c0531d992d6f06208992
MariosKtrs <mskatsaros@outlook.com>
2023-09-28 11:34:40 +0300

    docker-compose converted to dockerfiles for ex1

--- ASK1/WORDPRESS/exercise1/attack-container/build.sh
+++ ASK1/WORDPRESS/exercise1/attack-container/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker stop metasploit
 docker container rm metasploit
-docker-compose up -d
 docker build --tag=metasploit .
 docker run -d -p443:443 --rm --name=metasploit metasploit
 docker exec -it metasploit /bin/bash
