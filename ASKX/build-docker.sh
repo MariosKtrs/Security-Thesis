45fe0ce70d509ae2a3e89b18f7d12b791b254a5c
MariosKtrs <mskatsaros@outlook.com>
2023-11-03 16:31:45 +0200

    test

--- ASKX/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker stop buffer
-docker build --tag=buffer .
-docker run -d --security-opt seccomp=unconfined -p 1337:1337 --rm --name=buffer buffer
-docker exec -it buffer /bin/bash
