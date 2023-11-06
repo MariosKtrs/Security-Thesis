b8c4940544bd57d13d6127c46660cf231879f23c
MariosKtrs <mskatsaros@outlook.com>
2023-11-06 20:19:08 +0200

    bug fixes

--- ASK6/build.sh
+++ ASK6/build.sh
@@ -5,9 +5,4 @@ sudo docker build --tag=cryptography .
 sudo docker run -d -p 1337:1337 --rm --name=cryptography cryptography
 sudo docker exec -it cryptography /bin/bash
 
-sudo docker stop crypt
-sudo docker rm crypt
-sudo docker build --tag=crypt .
-sudo docker run -d -p 1337:1337 --name=crypt crypt
-sudo docker exec -it crypt /bin/bash
 
--- ASK6/stop.sh
+++ ASK6/stop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker stop crypt
+sudo docker stop cryptography
